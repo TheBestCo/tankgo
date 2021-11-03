2528de1c8dca65d2b5cf64c09b1958df32b36451
Konstantinos Pittas <konstantinos.pittas@gmail.com>
2021-11-03 15:33:19 +0200

    add testcontainers for testing

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/TheBestCo/tankgo
 
 go 1.16
 
-require github.com/stretchr/testify v1.7.0
+require (
+	github.com/docker/go-connections v0.4.0
+	github.com/stretchr/testify v1.7.0
+	github.com/testcontainers/testcontainers-go v0.11.1
+)
--- integration_test.go
+++ integration_test.go
@@ -1,16 +1,54 @@
 package tankgo
 
 import (
+	"context"
 	"log"
 	"testing"
 
 	"github.com/TheBestCo/tankgo/message"
 	"github.com/TheBestCo/tankgo/subscriber"
+	"github.com/docker/go-connections/nat"
 	"github.com/stretchr/testify/assert"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
-func TestIntegration(t *testing.T) {
+type TankContainer struct {
+	testcontainers.Container
+	Port nat.Port
+}
 
+func setupTankContainer(ctx context.Context) (*TankContainer, error) {
+	req := testcontainers.ContainerRequest{
+		Image:        "phaistos/tank",
+		ExposedPorts: []string{"11011"},
+		WaitingFor: wait.ForAll(
+			wait.ForLog("1 topics registered"),
+			wait.ForListeningPort("11011/tcp"),
+		),
+		Cmd: []string{
+			"/bin/sh", "-c",
+			"mkdir -p /data/test_topic/0; tank -p /data -l :11011",
+		},
+	}
+
+	c, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	p, err := c.MappedPort(ctx, "11011")
+	if err != nil {
+		return nil, err
+	}
+
+	return &TankContainer{Container: c, Port: p}, nil
+}
+
+func TestIntegration(t *testing.T) {
 	// f2 := message.ConsumeRequest{
 	// 	ClientVersion: 2,
 	// 	RequestID:     123,
@@ -31,6 +69,16 @@ func TestIntegration(t *testing.T) {
 	// 	},
 	// }
 
+	ctx := context.Background()
+
+	tankC, err := setupTankContainer(ctx)
+	assert.NoError(t, err)
+	defer tankC.Terminate(ctx)
+
+	// Send message
+	_, err = tankC.Exec(ctx, []string{"/usr/local/bin/tank-cli", "-t" ,"test_topic", "produce", "foo"})
+	assert.NoError(t, err)
+
 	f2 := message.ConsumeRequest{
 		ClientVersion: 2,
 		RequestID:     123,
@@ -39,7 +87,7 @@ func TestIntegration(t *testing.T) {
 		MinBytes:      0,
 		Topics: []message.FetchRequestTopic{
 			{
-				Name: "topic1",
+				Name: "test_topic",
 				Partitions: []message.FetchRequestTopicPartition{
 					{
 						PartitionID:       0,
@@ -51,7 +99,7 @@ func TestIntegration(t *testing.T) {
 		},
 	}
 
-	s, err := subscriber.NewSubscriber("127.0.0.1:11011")
+	s, err := subscriber.NewSubscriber("127.0.0.1:" + tankC.Port.Port())
 	//s, err := subscriber.NewSubscriber("192.168.10.235:11011")
 	assert.NoError(t, err)
 
