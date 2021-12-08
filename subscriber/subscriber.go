1e5cdc8aad9b0bec8378d7b83758b80b1fd4f0b7
Nikos Makropoulos <nmakro@gmail.com>
2021-12-08 10:50:25 +0200

    Remove comments

--- subscriber/subscriber.go
+++ subscriber/subscriber.go
@@ -13,17 +13,7 @@ import (
 )
 
 type Subscriber interface {
-	// Subscribe returns output channel with messages from provided topic.
-	// Channel is closed, when Close() was called on the subscriber.
-	//
-	// To receive the next message, `Ack()` must be called on the received message.
-	// If message processing failed and message should be redelivered `Nack()` should be called.
-	//
-	// When provided ctx is cancelled, subscriber will close subscribe and close output channel.
-	// Provided ctx is set to all produced messages.
-	// When Nack or Ack is called on the message, context of the message is canceled.
 	Subscribe(ctx context.Context, topic string) (<-chan *message.Message, error)
-	// Close closes all subscriptions with their output channels and flush offsets etc. when needed.
 	Close() error
 }
 
